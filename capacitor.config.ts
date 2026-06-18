1d54272306195a9c0086ad7332db7849b7077955
vincent.huang <yuanhong37@gmail.com>
2026-06-18 14:34:31 +0200

    chore: add linting support and update dependencies in package.json

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.poc.readhelper',
   appName: 'read-helper',
-  webDir: 'dist/read-helper'
+  webDir: 'dist/read-helper/browser'
 };
 
 export default config;
